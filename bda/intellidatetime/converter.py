2f4066589bb6bb3c11e8876eb195c0c0ce95e5ea
jensens <jensens@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-29 17:09:05 +0000

    added me

--- bda/intellidatetime/converter.py
+++ bda/intellidatetime/converter.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008, BDA - Blue Dynamics Alliance, Austria - www.bluedynamics.com
+# Copyright 2008, BlueDynamics Alliance, Austria - http://bluedynamics.com
 #
 # GNU General Public Licence Version 2 or later - see LICENCE.GPL
 
-__author__ = """Robert Niederreiter <rnix@squarewave.at>"""
+__author__ = """Robert Niederreiter <rnix@squarewave.at>,
+                Jens Klein <jens@bluedynamics.com>"""
 __docformat__ = 'plaintext'
 
 import types
