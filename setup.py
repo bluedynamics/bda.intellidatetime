7cc4db03c5a87651bc228f03a879371ca575b251
rnix <rnix@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-12-04 10:22:11 +0000

    README.txt to README.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 version = '1.1'
 shortdesc = 'bda.intellidatetime'
-longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(name='bda.intellidatetime',
       version=version,
